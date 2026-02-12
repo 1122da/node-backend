fdcc2c54b1301fbabf2a1fbb548b0c64ebe9fb2f
danish ijaz <dali19950@gmail.com>
2026-02-13 02:12:04 +0500

    Fix all issues

--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,17 @@
 const firebase = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
+const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+if (!base64) {
+  throw new Error(
+    'Missing FIREBASE_SERVICE_ACCOUNT_BASE64 in config.env. ' +
+      'Add the base64-encoded content of your service account JSON (run: node -e "console.log(require(\'fs\').readFileSync(\'./firebase/serviceAccountKey.json\').toString(\'base64\'))")'
+  );
+}
+
+const serviceAccount = JSON.parse(
+  Buffer.from(base64, 'base64').toString('utf8')
+);
+
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
 });
