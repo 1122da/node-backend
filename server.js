7dd6724c667a80e41f453c6cebb933ce09ccda05
danish ijaz <dali19950@gmail.com>
2026-02-13 02:39:28 +0500

    now try service accounst josn file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: './config.env' });
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, 'config.env') });
 const app = require('./app');
 const { Server } = require('socket.io');
 const http = require('http');
